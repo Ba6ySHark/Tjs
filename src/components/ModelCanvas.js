3f8643651a64b8f884ae3766605adc27c33913de
Ba6ySHark <steven.susorov@gmail.com>
2023-11-13 23:23:47 -0700

    Rendered Pillow model

--- src/components/ModelCanvas.js
+++ src/components/ModelCanvas.js
@@ -1,10 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import Pillow from "./Pillow";
+import Camera from "./Camera";
+import Backdrop from "./Backdrop";
+import { Environment, Center } from "@react-three/drei";
 
 export default function ModelCanvas() {
     return (
         <Canvas>
-            <Pillow/>
+            <ambientLight intensity={0.5} />
+            <Environment preset="city" />
+            <Camera>
+                
+                <Center>
+                    <Pillow/>
+                </Center>
+            </Camera>
         </Canvas>
     );
 }
